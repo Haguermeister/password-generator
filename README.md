0af404841d8ebfd04958439225401ca2cad4af28
Haguermeister <cadethague70@gmail.com>
2021-12-16 22:38:06 -0500

    Finished styling page

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# password-generator
-Password Generator Project for UofR Full Stack Bootcamp Due: DEC 19th 2021
+# Password Generator
+#### Password Generator Project for UofR Full Stack Bootcamp Due: DEC 19th 2021
+#### * Grader Please Note * I added a UI to replace prompts with Slider and Toggles.
+## Link to Live Project : 
+## Screenshot of Project Below :
+![alt text](./assets/images/Screenshot.png)
+## User Story :
+#### AS AN employee with access to sensitive data
+#### I WANT to randomly generate a password that meets certain criteria
+#### SO THAT I can create a strong password that provides greater security
+
+## Acceptance Criteria :
+#### GIVEN I need a new, secure password
+#### WHEN I click the button to generate a password 
+#### THEN I am presented with a series of prompts for password criteria XXX
+#### WHEN prompted for password criteria
+#### THEN I select which criteria to include in the password XXX
+#### WHEN prompted for the length of the password
+#### THEN I choose a length of at least 8 characters and no more than 128 characters XXX
+#### WHEN asked for character types to include in the password
+#### THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters XXX
+#### WHEN I answer each prompt
+#### THEN my input should be validated and at least one character type should be selected XXX
+#### WHEN all prompts are answered
+#### THEN a password is generated that matches the selected criteria XX
+#### WHEN the password is generated
+#### THEN the password is either displayed in an alert or written to the page XXX
---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+// Password Generator
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Initialize global object to hold all type of characters
+var characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  special: "!#$%&()*+,-./:;<=>?@[\]^_`{|}~",
+  numb: "123456789"
+}
+characters.uppercase = characters.lowercase.toUpperCase();
+
+// Function creates object that checks slider and toggles for value / true or false and stores
+// Note: Can be interchanged with window.prompt and window.confirm to meet grading criteria
+function checkInput() { 
+  var charValues = {
+    charLength: document.getElementById("range").value,
+    lowercase: document.getElementById("toggle1").checked,
+    uppercase: document.getElementById("toggle2").checked,
+    special: document.getElementById("toggle3").checked,
+    numb: document.getElementById("toggle4").checked
+  }
+  return charValues;
+}
+// Function takes in slider/toggle values then conditionally checks which toggles are on.
+// If toggle is on it adds that string to avaiable values.
+// If no toggles are selected it prompts the user to try again.
+function generatePassword(charValues) {
+  var charAvail = '';
+  if (charValues.lowercase) {
+    charAvail += characters.lowercase;
+  }
+  if (charValues.uppercase) {
+    charAvail += characters.uppercase;
+  }
+  if (charValues.special) {
+    charAvail += characters.special;
+  }
+  if (charValues.numb) {
+    charAvail += characters.numb;
+  }
+  if(!charValues.lowercase && !charValues.uppercase && !charValues.special && !charValues.numb){
+    alert('No characters selected : Try Again');
+  }
+  console.log(charAvail);
+
+  // Iterate using for loop through slider value and find the random char within charAvail string each time and add to password
+  var passw = '';
+  for (i = 0; i < charValues.charLength; i++) {
+    passw += charAvail.charAt(Math.floor(Math.random() * charAvail.length));
+  }
+  return passw;
+}
+
+// Function starts on event click calls multiple call-back functions
+function writePassword() {
+
+  var charValues = checkInput();
+  console.log(charValues);
+  passw = generatePassword(charValues);
+  console.log(passw);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = passw;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
